76f22b32e104d19a5f4c56e8527c26b2ff5f68a2
Aria Bennett <twoplustwo@gmail.com>
2013-12-31 10:54:15 -0800

    Extended _.extend()

--- src/underbar.js
+++ src/underbar.js
@@ -274,7 +274,6 @@ var _ = { };
     if (iterator === undefined) {
       var iterator = function(collection) {
         _.each(collection, function(item){
-          console.log(item);
           if (typeof(item) === "string" && item.length !== 0) {
             foundOne = true;
           }
@@ -317,6 +316,13 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    _.each(args, function(arg){
+      _.each(Object.keys(arg), function(key){
+        obj[key] = arg[key];
+      });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
